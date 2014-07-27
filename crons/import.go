fac37fe5758a5f9959b38498c3045401e196a029
Peekmo <axel.anceau@gmail.com>
2014-07-27 21:58:29 +0200

    Read gzipped content

--- crons/import.go
+++ crons/import.go
@@ -1,63 +1,114 @@
 package crons
 
 import (
+	"compress/gzip"
 	"fmt"
 	"github.com/revel/revel"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-type Import struct {
-}
+// Structure that implements the Job interface
+type Import struct{}
 
+// Run is the method called by the cronjob
+// It downloads the archive file and update the database
 func (this Import) Run() {
-	err := this.download()
+	// fullPath, err := this.download()
+	// if err != nil {
+	// 	revel.ERROR.Fatal(err)
+	// 	return
+	// }
+
+	fullPath := "imports/2014-07-27-1.json.gz"
+	data, err := this.ungzip(fullPath)
 	if err != nil {
 		revel.ERROR.Fatal(err)
-		return
 	}
+
+	this.parse(data)
 }
 
 // download Downloads the archive file from githubarchive
-func (this *Import) download() error {
+func (this *Import) download() (string, error) {
 	date := time.Now().Format("2006-01-02")
 	url := fmt.Sprintf("%s/%s-1.json.gz", revel.Config.StringDefault("imports.url", "http://data.githubarchive.org"), date)
 
 	tokens := strings.Split(url, "/")
 	file := tokens[len(tokens)-1]
 	folder := revel.Config.StringDefault("imports.folder", "imports")
+	fullPath := folder + "/" + file
 
 	// Checks if the folder exists
 	if _, err := os.Stat(folder); os.IsNotExist(err) {
 		if err := os.Mkdir(folder, 0660); err != nil {
-			return err
+			return fullPath, err
 		}
 	}
 
 	// Creates a file
 	revel.INFO.Printf("Creating file %s", file)
-	output, err := os.Create(folder + "/" + file)
+	output, err := os.Create(fullPath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer output.Close()
 
+	// Get the archive from githubarchive
 	revel.INFO.Println("Downloading the file...")
 	response, err := http.Get(url)
 	if err != nil {
-		return err
+		return fullPath, err
 	}
 	defer response.Body.Close()
 
+	// Write the file into the created one
 	revel.INFO.Println("Copying the file...")
 	bytes, err := io.Copy(output, response.Body)
 	if err != nil {
-		return err
+		return fullPath, err
 	}
 
 	revel.INFO.Printf("File's downloading done (%s bytes)", bytes)
+	return fullPath, nil
+}
+
+// ungzip ungzip the given gzipped file and returns its content
+func (this *Import) ungzip(file string) (string, error) {
+	// Read the file
+	fileReader, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer fileReader.Close()
+
+	reader, err := gzip.NewReader(fileReader)
+	if err != nil {
+		return "", err
+	}
+	defer reader.Close()
+
+	barray, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+
+	str := string(barray[:])
+	return str, nil
+}
+
+// parse the given json string and updates the database with it
+func (this *Import) parse(data string) error {
+	array := strings.Split(data, "\n")
+
+	for _, event := range array {
+		fmt.Println(event)
+		break
+	}
+
 	return nil
 }
